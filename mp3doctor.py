147075f6c84701631dabd91c0d1a528666c4f862
Paul Cowan <paul@custardsource.com>
2012-02-02 19:31:41 +1100

    Tidy up output, better structure.

--- mp3doctor.py
+++ mp3doctor.py
@@ -3,22 +3,26 @@ from tagger import *
 import sys, os, fnmatch
 
 class DirectoryCheck(object):
+  def __init__(self, tests):
+    self.tests = tests
+
   def check_dir(self, directory):
+    tocheck = []
     files = fnmatch.filter(os.listdir(directory), '*.mp3')
-    self.check_dir_files(directory, files)
-
-  def check_dir_files(self, directory, files):
-    pass
-
-class FileCheck(DirectoryCheck):
-  def check_dir_files(self, directory, files):
     for file in files:
       path = os.path.join(directory, file)
       id3 = ID3v2(path)
       if not id3.tag_exists():
         print(path, "Unable to find ID3v2 tag")
       else:
-        self.check_file(file, id3)
+        tocheck.append((path, id3))
+    for test in self.tests:
+      test.run_check(tocheck)
+
+class FileCheck(object):
+  def run_check(self, files):
+    for file, tags in files:
+      self.check_file(file, tags)
 
   def check_file(self, file, id3):
     pass
@@ -30,14 +34,13 @@ class TagPresentCheck(FileCheck):
   def check_file(self, file, id3):
     try:
       matchframe = [frame for frame in id3.frames if frame.fid == self.tag][0]
-      print "Got it %s" % (matchframe,)
+      # print "Got it %s" % (matchframe,)
     except IndexError:
       print(file, "No frame: %s" % self.tag)
 
-def runchecks(paths):
+def runchecks(path):
   tests = [TagPresentCheck('APIC'), TagPresentCheck('TALB')]
-  for path in paths:
-     for test in tests:
-       test.check_dir(path)
+  tester = DirectoryCheck(tests)
+  tester.check_dir(path)
 
-runchecks((sys.argv[1],))
+runchecks(sys.argv[1])
